123c03e9c931908bb8b47b601f6536c0c47f6d6b
Walter1014 <themarian1307@gmail.com>
2025-11-21 22:20:17 +0800

    Ok

--- backend/src/annotator/api/annotation/endpoints.py
+++ backend/src/annotator/api/annotation/endpoints.py
@@ -7,6 +7,10 @@ from annotator.api.annotation.business import (
     annotate,
 )
 
+from annotator.api.annotation.dto import (
+    annotation_generator,
+    annotation_getter,
+)
 
 
 annotation_ns = Namespace(name="annotation", validate=True)
@@ -15,16 +19,21 @@ annotation_ns = Namespace(name="annotation", validate=True)
 class Annotate(Resource):
     """Handles HTTP requests to URL: /api/v1/annotation/generate"""
 
+    @annotation_ns.expect(annotation_getter)
     @annotation_ns.doc(security="Bearer")
-    def get(self, file_uuid):
+    def get(self):
         """Retrieve message history"""
-
+        request_data = annotation_getter.parse_args()
+        file_uuid = request_data["file_uuid"]
         return get_annotation(file_uuid)
 
-
+    @annotation_ns.expect( annotation_generator)
     @annotation_ns.doc(security="Bearer")
-    def post(self, file_uuid, function_name):
+    def post(self):
         """Upload generated chat message to db"""
+        request_data = annotation_generator.parse_args()
+        file_uuid = request_data["file_uuid"]
+        function_name = request_data["function_name"]
         return annotate(file_uuid, function_name)
 
     # @annotation_ns.doc(security="Bearer")
